3d093c2fa7aa39f0947c974d4e8b34275d13e3e8
Itamar Amith <itamaram@gmail.com>
2020-12-14 14:44:53 +1100

    Use correct placeholder instead of name

--- src/inputs/DropdownInput.jsx
+++ src/inputs/DropdownInput.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { FormControl, FormGroup, FormLabel } from 'react-bootstrap';
 
 function DropdownInput(props) {
-    const { options, display, name } = props.definition;
+    const { options, display, placeholder } = props.definition;
     return (
         <FormGroup>
-            <FormLabel>{display || name}</FormLabel>
+            <FormLabel>{display || placeholder}</FormLabel>
             <FormControl as="select" defaultValue={props.value} onChange={e => props.onChange(e.target.value)}>
                 {[{ key: '' }, ...options].map(o =>
                     <option value={o.key} key={o.key}>{o.key}</option>
