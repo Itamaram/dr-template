e5cae543fa14bbd44776e66cae2f5eaf682e1734
Itamar Amith <itamaram@gmail.com>
2020-12-14 16:11:28 +1100

    Add default values

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class Container extends React.Component {
     // seed values
     const values = variables
       .filter(v => v.definition.placeholder)
-      .reduce((p, v) => Object.assign(p, { [v.definition.placeholder]: v.handler.seed }), {});
+      .reduce((p, v) => Object.assign(p, { [v.definition.placeholder]: v.definition.default || v.handler.seed }), {});
 
     return { variables, pattern, values };
   }
