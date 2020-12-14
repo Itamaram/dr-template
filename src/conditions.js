21a6ba9ce5a2fb3eb589516acfb2f353c55c5320
Itamar Amith <itamaram@gmail.com>
2020-12-14 15:59:23 +1100

    add more conditions

--- src/conditions.js
+++ src/conditions.js
@@ -1,3 +1,9 @@
 export default function assess(condition, values) {
-    return !condition || values[condition.field] === condition.equals
+    if (!condition)
+        return true;
+    if (condition.equals)
+        return values[condition.field] === condition.equals;
+    if (condition['not-equals'])
+        return values[condition.field] !== condition['not-equals'];
+    return false;
 }
