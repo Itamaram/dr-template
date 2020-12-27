09059e4770224aa571879ee44c065eb1fd499e97
Itamar Amith <itamaram@gmail.com>
2020-12-27 14:42:02 +1100

    Support recursive tree conditions

--- src/conditions.js
+++ src/conditions.js
@@ -1,6 +1,10 @@
 export default function assess(condition, values) {
     if (!condition)
         return true;
+    if (condition.and)
+        return condition.and.every(c => assess(condition, values));
+    if (condition.or)
+        return condition.or.some(c => assess(c, values));
     if (condition.equals)
         return values[condition.field] === condition.equals;
     if (condition['not-equals'])
