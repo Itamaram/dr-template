9f8171d0a32f977b2f17a1df1e4b0250d13905f3
Itamar Amith <itamaram@gmail.com>
2021-01-05 17:27:00 +1100

    Reset values as part of update, not render

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ class Container extends React.Component {
   constructor(props) {
     super(props);
     this.state = { tindex: 0, ...this.processTemplate(templates[0]) };
+
+    this.onChange = this.onChange.bind(this);
   }
 
   processTemplate(template) {
@@ -29,6 +31,38 @@ class Container extends React.Component {
     return { variables, pattern, values };
   }
 
+  arrayEquals(arr1, arr2) {
+    if (arr1.length !== arr2.length)
+      return false;
+
+    for (let i = 0; i < arr1.length; i++)
+      if (arr1[i] !== arr2[i])
+        return false;
+
+    return true;
+  }
+
+  onChange(key, value) {
+    this.setState(
+      { values: Object.assign(this.state.values, { [key]: value }) },
+      () => this.filterFields()
+    );
+  }
+
+  filterFields() {
+    const { variables, values } = this.state;
+
+    variables.forEach(({ definition }) => {
+      if (assess(definition.condition, values))
+        return
+
+      const target = [definition.default || []].flat();
+
+      if (!this.arrayEquals(values[definition.placeholder], target))
+        this.onChange(definition.placeholder, target);
+    });
+  }
+
   render() {
     return (
       <>
@@ -37,10 +71,10 @@ class Container extends React.Component {
             <Form.Row>
               <Form.Label className="col-2">Template:</Form.Label>
               <FormControl as="select" className="col-10" defaultValue={this.state.tindex}
-              onChange={e => this.setState({
-                tindex: e.target.value,
-                ...this.processTemplate(templates[e.target.value])
-              })}>
+                onChange={e => this.setState({
+                  tindex: e.target.value,
+                  ...this.processTemplate(templates[e.target.value])
+                })}>
                 {templates.map((t, i) => <option value={i} key={i}>{t.name}</option>)}
               </FormControl>
             </Form.Row>
@@ -49,7 +83,7 @@ class Container extends React.Component {
         <div className="row py-3">
           <div className="col-3">
             <div className="sticky-top">
-              <ControlsPane variables={this.state.variables} values={this.state.values} onChange={(key, value) => this.setState({ values: Object.assign(this.state.values, { [key]: value }) })} />
+              <ControlsPane variables={this.state.variables} values={this.state.values} onChange={this.onChange} />
             </div>
           </div>
           <div className="col">
@@ -64,7 +98,7 @@ class Container extends React.Component {
 }
 
 function TextResult(props) {
-  function getText({pattern, variables, values}) {
+  function getText({ pattern, variables, values }) {
     return process(
       pattern,
       variables.filter(({ definition }) => assess(definition.condition, values)),
--- src/controls-pane.js
+++ src/controls-pane.js
@@ -1,28 +1,7 @@
 import assess from './conditions';
 
-function arrayEquals(arr1, arr2) {
-    if (arr1.length !== arr2.length)
-        return false;
-
-    for (let i = 0; i < arr1.length; i++)
-        if (arr1[i] !== arr2[i])
-            return false;
-
-    return true;
-}
-
 export default function ControlsPane({ variables, values, onChange }) {
     return variables
-        .filter(({ definition }) => {
-            if (assess(definition.condition, values))
-                return true;
-
-            const target = [definition.default || []].flat();
-
-            if (!arrayEquals(values[definition.placeholder], target))
-                onChange(definition.placeholder, target);
-
-            return false;
-        })
+        .filter(({ definition }) => assess(definition.condition, values))
         .map(({ definition, handler }) => handler.render(definition, values[definition.placeholder], x => onChange(definition.placeholder, x)))
 }
