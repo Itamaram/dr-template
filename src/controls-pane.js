1fbaca3dab16d8688fc7a091b3368bf15620632d
Iftah Amith <iftah.amith@gmail.com>
2023-07-02 20:02:33 +1000

    fixes

--- src/controls-pane.js
+++ src/controls-pane.js
@@ -16,11 +16,11 @@ export default function ControlsPane({ variables, values, onChange }) {
   };
 
   function findNextMediumStyleIndex(startIndex) {
-    const filteredVariables = variables.filter(({ definition }) => assess(definition.condition, values));
+    const filteredVariables = variables;
 
     for (let i = startIndex + 1; i < filteredVariables.length; i++) {
-      const { definition, handler } = filteredVariables[i];
-      if (definition && handler.type === 'title' && definition.style === 'medium') {
+      const { definition} = filteredVariables[i];
+      if (definition && definition.style === 'medium') {
         return i;
       }
     }
@@ -30,7 +30,7 @@ export default function ControlsPane({ variables, values, onChange }) {
 
   const minimizeButtonHandler = () => {
     const indicesToMinimize = variables.reduce((indices, { definition }, index) => {
-    if (definition ) {
+    if (definition) {
         indices.push(index);
     }
     return indices;
@@ -56,13 +56,18 @@ export default function ControlsPane({ variables, values, onChange }) {
         </div>
       {variables
         .filter(({ definition }) => assess(definition.condition, values))
-        .map(({ definition, handler }, index) => {
+        .map(({ definition, handler }, originalIndex) => {
+            const filteredVariables = variables
+            .map((variable, originalIndex) => ({ ...variable, originalIndex })) // Add index property to each variable
+            .filter(({ definition }) => assess(definition.condition, values));
+
+            originalIndex = filteredVariables[originalIndex].originalIndex;
           if (definition && definition.style === 'medium') {
-            const isMinimized = minimizeIndices.includes(index);
+            const isMinimized = minimizeIndices.includes(originalIndex);
             const titleStyle = isMinimized ? { borderLeft: '4px double #61dafb' } : {};
 
             return (
-              <div key={definition.placeholder} onClick={() => handleTitleClick(index)}>
+              <div key={definition.placeholder} onClick={() => handleTitleClick(originalIndex)}>
                 <div style={titleStyle}>
                   {handler.render(definition, values[definition.placeholder], x =>
                     onChange(definition.placeholder, x)
@@ -70,7 +75,7 @@ export default function ControlsPane({ variables, values, onChange }) {
                 </div>
               </div>
             );
-          } else if (minimizeIndices.includes(index)) {
+          } else if (minimizeIndices.includes(originalIndex)) {
             return null; // Return null to hide the control component
           } else {
             return handler.render(definition, values[definition.placeholder], x =>
