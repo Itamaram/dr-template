308158d7c40bb5ea9fba121b46c84c9f24863648
Iftah Amith <iftah.amith@gmail.com>
2023-07-05 10:49:39 +1000

    number control

---
+++ src/controls/NumberControl.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FormControl, FormGroup, FormLabel, Row, Col } from 'react-bootstrap';
+
+function NumberControl(props) {
+  const { display, placeholder, hide, text } = props.definition;
+  const value = props.values[0] || '';
+
+  // Remove the text part from the value
+  const numericValue = value.split(' ')[0];
+
+  return hide || (
+    <FormGroup>
+      <FormLabel>{display || placeholder}</FormLabel>
+      <Row>
+        <Col xs={9}>
+          <FormControl
+            value={numericValue} // Only include the numeric value in the text box
+            type="text"
+            onChange={e => {
+              const inputValue = e.target.value;
+              const numericValue = inputValue.replace(/[^0-9.]/g, '');
+
+              // Check if numericValue is empty before concatenating with text
+              const valueWithText = numericValue ? `${numericValue} ${text}` : '';
+
+              props.onChange([valueWithText]); // Include the concatenated value in the onChange callback
+            }}
+            pattern="[0-9.]*"
+            inputMode="decimal"
+          />
+        </Col>
+        <Col xs={3} className="d-flex align-items-center">
+          <span>{text}</span>
+        </Col>
+      </Row>
+    </FormGroup>
+  );
+}
+
+
+
+export const handler = {
+  type: 'number',
+  render: function (definition, current, onChange) {
+    return <NumberControl definition={definition} values={current} onChange={onChange} key={definition.placeholder} />;
+  },
+  getValues: function (variable, values, mod) {
+    if (values && values.length > 0) {
+      return values[0];
+    }
+    return '';
+  }
+};
--- src/handlers.js
+++ src/handlers.js
@@ -6,10 +6,11 @@ import { handler as title } from './controls/TitleControl'
 import { handler as select } from './controls/SelectControl'
 import { handler as date } from './controls/DateControl'
 import { handler as time } from './controls/TimeControl'
-import {handler as datetime} from './controls/DateTimeControl'
+import { handler as datetime } from './controls/DateTimeControl'
 import { handler as textmulti } from './controls/TextMultiControl'
+import {handler as number } from './controls/NumberControl'
 
-const handlers = [checkbox, dropdown, radio, text, title, select, date, time, datetime, textmulti]
+const handlers = [checkbox, dropdown, radio, text, title, select, date, time, datetime, textmulti, number]
     .reduce((p, h) => Object.assign(p, { [h.type]: h }), {});
 
 export default handlers;
